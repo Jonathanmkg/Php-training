b7c31267006234bc1e4f3d99b3c668caad6de5ba
jonathanmkg <bantumukenge@gmail.com>
2024-06-04 11:14:55 +0200

    rate exchange file added

---
+++ rate_exchange.php.php
@@ -0,0 +1,45 @@
+<?php
+
+function convertdollarstofrancs($rate, $amount){
+    $result= $rate *$amount;
+    return $result;
+}
+
+function convertfrancstodollars($rate, $amount){
+    $result= $amount/ $rate;
+    return $result;
+}
+
+$amount = $_POST["amount"] ?? null;
+$rate = $_POST["rate"] ?? null;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rate</title>
+</head>
+<body>
+   <h1>Rate exchange of dollars to francs congolais</h1>
+   <form action="" method="post">
+        <input type="number" name="rate" placeholder= "insert the value 1$ in fc" required>
+        <input type="text" name="rate" placeholder="insert the amount in fc" required>
+        <button type="submit">convert</button>
+   </form> 
+
+   <p>Your money in fc is </p>= convertdollarstofrancs($rate, $amount) ?> FC</p>
+
+   <h1>exchange of francs to dollars</h1>
+   <form action= "" method="post">
+        <input type="number" name="rate" placeholder= "insert the value 1$ in fc" required>
+        <input type="text" name="rate" placeholder="insert the amount in fc" required>
+        <button type="submit">convert</button>  
+   </form>
+
+   <p>Your amount in dollars is :<?= $rate == 0 ? "division by zero" : number_format(convertdollarstofrancs($rate, $amount),1) ?> </p>
+   
+</body>
+</html>
